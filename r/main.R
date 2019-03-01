358275ac0155804f165f4abc11663f152716de5e
Dante van der Heijden <dante@scholica.com>
2019-03-01 15:41:02 +0100

    Added Metrics Page

--- r/main.R
+++ r/main.R
@@ -4,4 +4,17 @@ if(!exists("utilize", mode="function")){
   utilize()
 }
 
-# Now that we have 
+dance_hist_times=c(nrow(pharrell), nrow(top90s), nrow(top10s))
+dance_hist <- data.frame(
+    Playlist=rep(c("Pharrell", "Top90s", "Top10s"), times=dance_hist_times),
+                  Danceability=c(pharrell$danceability, top90s$danceability, top10s$danceability)
+                )
+
+# Histograms for Danceability
+ggplot(dance_hist, aes(x=Danceability, color=Playlist, fill=Playlist)) +
+  geom_density() + 
+  scale_color_manual(values=c("#FF000A", "#FF8700", "#00C9F8")) +
+  scale_fill_manual(values=c("#FF000A", "#FF8700", "#00C9F8")) +
+  ylab("") + 
+  xlab("") +
+  theme_classic() 
